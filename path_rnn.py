25e3f19a2b4176bd485e9c96a2acd338543e793d
Conor McGrory <cpmcgrory@gmail.com>
2022-04-14 19:07:33 -0400

    Removed bias

--- path_rnn.py
+++ path_rnn.py
@@ -12,25 +12,20 @@ class PathRNN(torch.nn.Module):
         self.n_units = n_units
 
         # RNN Layer
-        self.rnn = torch.nn.RNN(input_size=2, hidden_size=n_units, num_layers=1, nonlinearity='tanh', batch_first=True)
+        self.rnn = torch.nn.RNN(input_size=2, hidden_size=n_units, num_layers=1, nonlinearity='tanh', batch_first=True, bias=False)
 
         # Output layer
-        self.output = torch.nn.Linear(n_units, 2)
+        self.output = torch.nn.Linear(n_units, 2, bias=False)
 
         # Initialize RNN weights
         for name, param in self.rnn.named_parameters():
-            if name == 'bias_ih_l0':
-                 torch.nn.init.zeros_(param)
-            elif name == 'bias_hh_l0':
-                 torch.nn.init.zeros_(param)
-            elif name == 'weight_ih_l0':
+            if name == 'weight_ih_l0':
                  torch.nn.init.normal_(param, mean=0.0, std=np.sqrt(1/2))
             elif name == 'weight_hh_l0':
                  torch.nn.init.orthogonal_(param)
 
         # Initialize output weights
         torch.nn.init.zeros_(self.output.weight.data)
-        torch.nn.init.zeros_(self.output.bias.data)
 
     def init_hidden(self, batch_size):
 
