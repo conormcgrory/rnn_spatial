66719178226b0b902e0de52f66c13f9e9855dbba
Conor McGrory <cpmcgrory@gmail.com>
2022-05-01 12:23:34 -0400

    Added metabolic loss function

--- trainer.py
+++ trainer.py
@@ -10,13 +10,50 @@ from trajectory import TrajectoryGenerator
 from model import PathRNN
 
 
+class TrainingLoss(torch.nn.Module):
+    
+    lambda_w: float
+    lambda_h: float
+
+    def __init__(self, lambda_w, lambda_h):
+        super(TrainingLoss, self).__init__()
+        self.lambda_w = lambda_w
+        self.lambda_h = lambda_h
+
+    def forward(self, pos, h, pos_est, w_ih, w_out):
+
+        # Mean squared error component of loss
+        loss_mse = torch.mean(torch.square(pos_est - pos))
+
+        # Weight regularization component of loss
+        L2_ih = torch.mean(torch.square(w_ih))
+        L2_out = torch.mean(torch.square(w_out))
+        loss_w = L2_ih + L2_out
+
+        # Metabolic component of loss
+        loss_h = torch.mean(torch.square(h))
+
+        # Total loss 
+        return loss_mse + self.lambda_w * loss_w + self.lambda_h * loss_h
+
+
 @dataclasses.dataclass
 class TrainerParams:
 
+    # Number of batches to run training for
     n_batches: int = 1900
+
+    # Number of trials per batch
     batch_size: int = 500
+
+    # Coefficient of L2 regularization term in loss function
+    lambda_w: float = 0.5
+
+    # Coefficient of metabolic term in loss function
+    lambda_h: float = 0.1
+
+    # Learning rate used for optimization
     learning_rate: float = 1e-4
-    weight_decay: float = 1e-4
 
 
 class Trainer:
@@ -32,14 +69,13 @@ class Trainer:
         # Model
         self.model = model
 
-        # Loss function
-        self.criterion = torch.nn.MSELoss()
+        # Loss
+        self.criterion = TrainingLoss(lambda_w=params.lambda_w, lambda_h=params.lambda_h)
 
         # Optimizer
         self.optimizer = torch.optim.Adam(
             self.model.parameters(), 
-            lr=self.params.learning_rate, 
-            weight_decay=self.params.weight_decay
+            lr=self.params.learning_rate,
         )
 
     def train(self):
@@ -54,15 +90,16 @@ class Trainer:
             # Clear gradients from previous batch
             self.optimizer.zero_grad()
 
-            # Compute loss
-            pos_est, _ = self.model(vel)
-            loss = self.criterion(pos_est, pos)
+            # Predict position for batch
+            pos_est, h = self.model(vel)
 
+            # Compute loss using predictions, activations, and weights
+            w_ih = self.model.rnn.weight_ih_l0.data
+            w_out = self.model.output.weight.data
+            loss = self.criterion(pos, h, pos_est, w_ih, w_out)
+ 
             # Compute gradient via backprop
             loss.backward()
-
-            # Gradient clipping
-            #torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=2.0, norm_type=2)
 
             # Update model parameters
             self.optimizer.step()
